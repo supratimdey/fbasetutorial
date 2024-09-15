cd35e75afb3922e6d88fd0c91a79dd6014c636f7
Supratim Dey <supratimdey@outlook.com>
2024-09-15 20:35:59 +0530

    Initial Commit

---
+++ fbasetutorial/fbasetutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  fbasetutorialApp.swift
+//  fbasetutorial
+//
+//  Created by dev on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct fbasetutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
